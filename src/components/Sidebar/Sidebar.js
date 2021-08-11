3d3f131783a86e9fe75962769bb85a1f4fdcd6b6
Rod Elliott <Rod.Elliott@lehighoutfitters.com>
2021-08-11 11:56:40 -0400

    Getting rid of a few items

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,26 +1,7 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, { Component } from "react";
 import { useLocation, NavLink } from "react-router-dom";
-
 import { Nav } from "react-bootstrap";
-
-import logo from "assets/img/reactlogo.png";
+import sideBarImage1 from "assets/img/sidebar-1.jpg";
 
 function Sidebar({ color, image, routes }) {
   const location = useLocation();
@@ -32,24 +13,18 @@ function Sidebar({ color, image, routes }) {
       <div
         className="sidebar-background"
         style={{
-          backgroundImage: "url(" + image + ")",
+          backgroundImage: "url(" + 'imageassets/img/sidebar-1.jpg' + ")",
         }}
       />
       <div className="sidebar-wrapper">
         <div className="logo d-flex align-items-center justify-content-start">
           <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
+            href="#"
             className="simple-text logo-mini mx-1"
           >
-            <div className="logo-img">
-              <img
-                src={require("assets/img/reactlogo.png").default}
-                alt="..."
-              />
-            </div>
           </a>
-          <a className="simple-text" href="http://www.creative-tim.com">
-            Creative Tim
+          <a className="simple-text" href="#">
+            Lehigh
           </a>
         </div>
         <Nav>
@@ -81,5 +56,4 @@ function Sidebar({ color, image, routes }) {
     </div>
   );
 }
-
 export default Sidebar;
